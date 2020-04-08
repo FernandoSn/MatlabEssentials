c864b31433f30bbba144220a68289b216172e66b
Fernando Gabriel Santos Valencia <fs121@duke.edu>
2020-04-08 11:38:50 -0400

    fixed kilosort2cpp. It was excluding unit 1 because in bolding's code unit 1 was the sum of all units.

--- Fer/Spike Trains Correlations/Kilosort2CppCorr.m
+++ Fer/Spike Trains Correlations/Kilosort2CppCorr.m
@@ -7,6 +7,9 @@ function Spikes = Kilosort2CppCorr(s,filename)
 %%blackrock data. This is useful because SpikeTrainAnalysis in C++ only
 %%takes data formatted in that way.
 
+%%the second section writes the data to a .dat file taking the same units
+%%as reference and as target.
+
 NoUnits = size(s.cids,2);
 
 sscarray = cell(NoUnits,1);
@@ -37,8 +40,10 @@ end
     Valves = 1;%Actually valves can be the number of different stimuli presented.
     Concentrations = 1; %provisional
     Trials = 1;
-    UnitsRef = size(SpikesRef.tsec,1) - 1;
-    UnitsTar = size(SpikesTar.tsec,1) - 1;
+%     UnitsRef = size(SpikesRef.tsec,1) - 1;
+%     UnitsTar = size(SpikesTar.tsec,1) - 1;
+    UnitsRef = size(SpikesRef.tsec,1);
+    UnitsTar = size(SpikesTar.tsec,1);
 % else
 %     Valves = size(efd.ValveTimes.FVSwitchTimesOn,1);%Actually valves can be the number of different stimuli presented.
 %     Concentrations = 1; %provisional
@@ -57,19 +62,19 @@ fwrite(fid,Trials,'uint16');
 fwrite(fid,UnitsRef,'uint16');
 fwrite(fid,UnitsTar,'uint16');
 
-for ii = 2:UnitsRef+1
+for ii = 1:UnitsRef
     fwrite(fid,size(SpikesRef.tsec{ii},1),'uint32');
 end
 
-for ii = 2:UnitsTar+1
+for ii = 1:UnitsTar
     fwrite(fid,size(SpikesTar.tsec{ii},1),'uint32');
 end
 
-for ii = 2:UnitsRef+1
+for ii = 1:UnitsRef
     fwrite(fid,SpikesRef.tsec{ii},'uint32');
 end
 
-for ii = 2:UnitsTar+1
+for ii = 1:UnitsTar
     fwrite(fid,SpikesTar.tsec{ii},'uint32');
 end
 
